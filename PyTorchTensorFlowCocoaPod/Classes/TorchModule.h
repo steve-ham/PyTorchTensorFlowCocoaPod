c4ed54c702771e85c73dadf6829264d9010d8f36
Steve Ham <steve.ham@kakaomobility.com>
2022-12-19 21:49:24 +0900

    0.1.2

---
+++ PyTorchTensorFlowCocoaPod/Classes/TorchModule.h
@@ -0,0 +1,16 @@
+//
+//  TorchModule.h
+//  PyTorchTensorFlowCocoaPod
+//
+//  Created by steve.ham on 2022/12/19.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TorchModule : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PyTorchTensorFlowCocoaPod/Classes/TorchModule.m
@@ -0,0 +1,12 @@
+//
+//  TorchModule.m
+//  PyTorchTensorFlowCocoaPod
+//
+//  Created by steve.ham on 2022/12/19.
+//
+
+#import "TorchModule.h"
+
+@implementation TorchModule
+
+@end
